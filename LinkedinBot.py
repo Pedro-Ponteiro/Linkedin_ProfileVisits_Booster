1ac21a9ebe150be1d23d5df5156f7c58e3c98cbf
Pedro-Ponteiro <48108738+Pedro-Ponteiro@users.noreply.github.com>
2020-07-04 18:39:56 -0300

    Update LinkedinBot.py

--- LinkedinBot.py
+++ LinkedinBot.py
@@ -5,7 +5,7 @@ import time
 
 
 desktop = os.path.expanduser("~/Desktop")
-pat = os.path.join(desktop, r"ArquivosPATH\chromedriver.exe")
+pat = os.path.join(desktop, r"ArquivosPATH\chromedriver2.exe")
 
 arquivo = "minhalista.txt"
 link1 = "https://www.linkedin.com/mynetwork/"
@@ -13,9 +13,8 @@ link2 = "https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww%2
 login = ""
 senha = ""
 
-pessoas = 300
+pessoas = 280
 vezes = 3
-
 
 
 def real():
@@ -43,19 +42,40 @@ def real():
 
     bod = k.find_element_by_xpath("//body")
 
-    links = []
-    print(f"Número de 'descidas': {int(pessoas / 3)}")
-    for i in range(0, int(pessoas/10)):
 
+    cont = 0
+    links = []
+    step = 10
+    while True:
+        cont += 1
         bod.send_keys(Keys.LEFT_CONTROL + Keys.END)
         time.sleep(1)
+        if cont == 1 or cont%step == 0:
+            links.clear()
+            nam = k.find_elements_by_xpath(inp_xpath)
+            for pess in nam:
+                if pess.get_attribute("href") not in atingidos:
+                    links.append(pess.get_attribute("href"))
+            if len(links) >= pessoas:
+                print("Step 1 complete")
+                break
 
-    nam = k.find_elements_by_xpath(inp_xpath)
-    print(f"numero de resultados: {len(nam)}")
-    for pess in nam:
-        if pess.get_attribute("href") not in atingidos:
-            links.append(pess.get_attribute("href"))
-    print(f"Número de resultados para pesquisa: {len(links)}")
+
+    # print(f"Número de 'descidas': {pessoas}")
+    # for i in range(0, pessoas):
+    #
+    #     bod.send_keys(Keys.LEFT_CONTROL + Keys.END)
+    #     time.sleep(1)
+    #
+    # nam = k.find_elements_by_xpath(inp_xpath)
+    # print(f"numero de resultados: {len(nam)}")
+    # for pess in nam:
+    #     if pess.get_attribute("href") not in atingidos:
+    #         links.append(pess.get_attribute("href"))
+    # print(f"Número de resultados para pesquisa: {len(links)}")
+
+
+
     # links.clear()
     # links.append("https://www.linkedin.com/in/jair-prado-03787343/")
     with open(arquivo, "a") as alvos:
@@ -64,9 +84,20 @@ def real():
             k.execute_script("window.open('');")
             k.switch_to.window(k.window_handles[1])
             k.get(pess)
-            print(links.index(pess))
-            time.sleep(2)
-            bodi = k.find_element_by_xpath("//body")
+
+            try:
+                bodi = k.find_element_by_xpath("//body")
+
+            except:
+                print(f"Stopped at {links.index(pess)}")
+                break
+            try:
+                test = k.find_element_by_xpath("//a[@data-tracking-control-name='public_profile_nav-header-signin']")
+                print(f"Stopped at {links.index(pess)}")
+                break
+            except:
+                print(links.index(pess))
+
 
             for i in range(0,3):
                 bodi.send_keys(Keys.PAGE_DOWN)
